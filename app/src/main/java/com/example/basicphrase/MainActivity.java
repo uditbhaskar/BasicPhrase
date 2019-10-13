d044e0e3de182f32f5d73961663a9dd3c6d052ff
UDIT BHASKAR <me_udit@udit.udit>
2019-10-13 18:13:44 +0530

    First commit

---
+++ app/src/main/java/com/example/basicphrase/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.basicphrase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+  MediaPlayer mediaPlayer;
+    int phraseNumber=0;
+
+    public void onClick(View view) {
+        Button button = (Button) view;
+
+        Log.i("PHRASE",button.getTag().toString() );
+        mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(button.getTag().toString(), "raw", getPackageName()));
+
+        mediaPlayer.start();
+
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
